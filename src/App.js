a171ed12534b6ff43cb50dba085732703b55b993
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-11-22 12:55:05 -0800

    finished hooks exercises

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+function App (props){
+
+  let [list, setList] = useState(["Ready", "Set", "GO!"]);
+  let [text,setText] = useState([""]);
+
+function onSubmit(event){
+event.preventDefault();
+setList([...list, text]);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Ghibli Studios</h1>
+      <h3>Search for your favorite film titles here:</h3>
+      <form onSubmit={onSubmit}>
+      <input
+            type="text"
+            value={text}
+            onChange={(event) => {
+              setText(event.target.value);
+            }} />
+            <button type="submit">Add</button>
+      </form>
+      <ul>
+        {list.map((ele, index) => 
+          <li key={index}>{ele}</li>
+        )}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/FilmsList.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './FilmsList.css';
+
+function FilmsList (props){
+
+    const [list, setList] = useState([]);
+
+async function getFilms (){
+    try{
+        let res = await fetch('https://ghibliapi.herokuapp.com/films');
+        let films = await res.json();
+        setList(films);
+    } catch (e){
+        console.error(e);
+    }
+}
+
+useEffect(() => {
+    getFilms();
+}, []);
+
+    
+        return (
+        <ul>
+            {list.map((ele) => {
+               return <li key={ele.id}><h2>{ele.title}</h2> <br /> 
+               <img src={ele.image} alt="Movie Poster" className="FilmsList-img" /> 
+               <br /> <span className='light-text'><i>{ele.description}</i></span> 
+               </li>  
+            })};
+            
+        </ul>
+        ); 
+}
+export default FilmsList;
